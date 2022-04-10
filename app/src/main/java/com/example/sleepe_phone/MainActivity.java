516e287704147fbcd1d77ca2c27bcc4d58eed7f8
Henry Luker <henry@hclu.co>
2022-04-09 19:52:15 -0500

    permissions work

--- app/src/main/java/com/example/sleepe_phone/MainActivity.java
+++ app/src/main/java/com/example/sleepe_phone/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.sleepe_phone;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
-
-import android.os.Bundle;
-
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    // Register the permissions callback, which handles the user's response to the
+    // system permissions dialog. Save the return value, an instance of
+    // ActivityResultLauncher, as an instance variable.
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,32 +37,34 @@ public class MainActivity extends AppCompatActivity {
         final Fragment left = new FeedbackFragment();
         final Fragment middle = new ReportsFragment();
         final Fragment right = new SettingsFragment();
-        findViewById(R.id.feedbackButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fm.beginTransaction();
-                fragmentTransaction.replace(R.id.layout, left);
-                fragmentTransaction.commit();
-            }
+        findViewById(R.id.feedbackButton).setOnClickListener(v -> {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            fragmentTransaction.replace(R.id.layout, left);
+            fragmentTransaction.commit();
         });
-        findViewById(R.id.reportsButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fm.beginTransaction();
-                fragmentTransaction.replace(R.id.layout, middle);
-                fragmentTransaction.commit();
-            }
+        findViewById(R.id.reportsButton).setOnClickListener(v -> {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            fragmentTransaction.replace(R.id.layout, middle);
+            fragmentTransaction.commit();
         });
-        findViewById(R.id.settingsButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fm.beginTransaction();
-                fragmentTransaction.replace(R.id.layout, right);
-                fragmentTransaction.commit();
-            }
+        findViewById(R.id.settingsButton).setOnClickListener(v -> {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            fragmentTransaction.replace(R.id.layout, right);
+            fragmentTransaction.commit();
         });
+
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACTIVITY_RECOGNITION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            // You can use the API that requires the permission.
+        } else {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+            requestPermissionLauncher.launch(
+                    Manifest.permission.ACTIVITY_RECOGNITION);
+        }
     }
 }
